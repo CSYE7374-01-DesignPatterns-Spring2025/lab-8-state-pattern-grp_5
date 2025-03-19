34017f13534a6a7baeb9a45b91c29adfb8fd0c92
Sampurna <sampurng@gmail.com>
2025-03-18 20:13:16 -0400

    Lunch State

---
+++ src/main/java/edu/neu/csye7374/LunchState.java
@@ -0,0 +1,11 @@
+package edu.neu.csye7374;
+
+public class LunchState {
+    @Override
+    public void showMenu() {
+        System.out.println("Lunch Menu:");
+        System.out.println("1 - Hot Dog   - $1.99");
+        System.out.println("2 - Salad     - $2.99");
+        System.out.println("3 - Hamburger - $3.99");
+    }
+}
